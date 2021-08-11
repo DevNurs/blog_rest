ca77d3fcd77dab01562ca20305c7af7700c8a507
Saifullakh1 <saifullakh7@gmail.com>
2021-08-11 08:20:37 +0600

    Count like

--- apps/posts/serializers.py
+++ apps/posts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from apps.posts.models import Post, PostImage, Like
+from rest_framework.fields import SerializerMethodField
 
 
 class PostImageSerializer(serializers.ModelSerializer):
@@ -10,10 +11,14 @@ class PostImageSerializer(serializers.ModelSerializer):
 
 class PostSerializer(serializers.ModelSerializer):
     post_images = PostImageSerializer(read_only=True, many=True)
+    like_post = SerializerMethodField()
 
     class Meta:
         model = Post
         fields = "__all__"
+
+    def get_like_post(self, instance):
+        return instance.like_post.all().count()
 
 
 class LikeSerializer(serializers.ModelSerializer):
